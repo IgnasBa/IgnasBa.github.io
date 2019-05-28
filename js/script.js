82d0633ad9426558713d15bf314b5cfce2e8e7e1
IgnasBa <43188272+IgnasBa@users.noreply.github.com>
2019-05-28 16:31:25 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,112 +1,122 @@
 /* Navigation from materialize.js */
 
-  $(document).ready(function(){
+$(document).ready(function () {
     $('.sidenav').sidenav();
-  });
-  
-  $(document).ready(function(){
+});
+
+/* Navigation from materialize.js */
+
+$(document).ready(function () {
+    /* Collapsible from materialize.js */
+
     $('.collapsible').collapsible();
-      $(".rotate1").click(function () {
-    $(".rotate11").toggleClass("down");
-          $(".rotate33").removeClass("down");
-           $(".rotate22").removeClass("down");
-           $(".rotate44").removeClass("down");
+
+    /* Collapsible from materialize.js */
+
+    /* Making arrows rotate on collapsible */
+
+    $(".rotate1").click(function () {
+        $(".rotate11").toggleClass("down");
+        $(".rotate33").removeClass("down");
+        $(".rotate22").removeClass("down");
+        $(".rotate44").removeClass("down");
+    });
+    $(".rotate2").click(function () {
+        $(".rotate22").toggleClass("down");
+        $(".rotate11").removeClass("down");
+        $(".rotate33").removeClass("down");
+        $(".rotate44").removeClass("down");
+    });
+    $(".rotate3").click(function () {
+        $(".rotate33").toggleClass("down");
+        $(".rotate11").removeClass("down");
+        $(".rotate22").removeClass("down");
+        $(".rotate44").removeClass("down");
+    });
+    $(".rotate4").click(function () {
+        $(".rotate44").toggleClass("down");
+        $(".rotate11").removeClass("down");
+        $(".rotate22").removeClass("down");
+        $(".rotate33").removeClass("down");
+    });
 });
-       $(".rotate2").click(function () {
-    $(".rotate22").toggleClass("down");
-           $(".rotate11").removeClass("down");
-           $(".rotate33").removeClass("down");
-           $(".rotate44").removeClass("down");
-});
-       $(".rotate3").click(function () {
-    $(".rotate33").toggleClass("down");
-           $(".rotate11").removeClass("down");
-           $(".rotate22").removeClass("down");
-           $(".rotate44").removeClass("down");
-});
-       $(".rotate4").click(function () {
-    $(".rotate44").toggleClass("down");
-           $(".rotate11").removeClass("down");
-           $(".rotate22").removeClass("down");
-           $(".rotate33").removeClass("down");
-});
-  });
-function ipLookUp () {
-  $.ajax('http://worldtimeapi.org/api/ip')
-  .then(
-      function success(response) {
-          console.log('User\'s Country', response.timezone);
-          $.ajax('http://api.timezonedb.com/v2.1/convert-time-zone?key=WRGOM452TQQY&format=json&from='+ response.timezone + '&to=Europe/Copenhagen')
-          .then( function success(response2)
-                {
-              console.log(response2.offset);
-              // Create a new JavaScript Date object based on the timestamp
-// multiplied by 1000 so that the argument is in milliseconds, not seconds.
-var date = new Date(response2.toTimestamp*1000);
-// Hours part from the timestamp
-var hours = date.getHours();
+
+/* Making arrows rotate on collapsible */
+
+/* Using TimezoneDB api to get current time in Denmark */
+function Time() {
+
+    $.ajax('http://api.timezonedb.com/v2.1/convert-time-zone?key=WRGOM452TQQY&format=json&from=Europe/Vilnius&to=Europe/Copenhagen')
+        .then(function success(response2) {
+                // Create a new JavaScript Date object based on the timestamp
+
+                // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+                var date = new Date(response2.toTimestamp * 1000);
+
+                // Hours part from the timestamp
+                var hours = date.getHours();
+                /* Showing if ScandiMate is available for calls, depending on current time */
+
+                if (hours > 8 && hours < 20) {
+                    document.getElementById("call").innerHTML = "00 45 53 32 32 52 (Available to call)";
+                } else {
+                    document.getElementById("call").innerHTML = "00 45 53 32 32 52 (Not available to call)";
+                }
+                /* Showing if ScandiMate is available for calls, depending on current time */
+            }
 
 
-              if(hours > 8 && hours < 20){
-              document.getElementById("call").innerHTML= "00 45 53 32 32 52 (Available to call)";}
-              else {
-                  document.getElementById("call").innerHTML= "00 45 53 32 32 52 (Not available to call)";}
-              }
-          
-          
-          )
-      },
-
-      function fail(data, status) {
-          console.log('Request failed.  Returned status of',
-                      status);
-      }
-  );
+        )
 }
-ipLookUp();
-$(function() {
-  //if ($(window).width() < 768) {
-   $('#contact').click(function() {
-    $('#contactForm2').fadeToggle();
-  })
-     $('#close').click(function() {
-    $('#contactForm').fadeToggle();
-  })
-     
-//}
-//else {
+/* Using TimezoneDB api to get current time in Denmark */
 
-   $('#contact2').click(function() {
-    $('#contactForm').fadeToggle();
-  })
-     $('#close2').click(function() {
-    $('#contactForm2').fadeToggle();
-  })
-//}
+Time();
+
+
+/* Using jQuery fadeToggle function to make the contact form appear */
+$(function () {
+    $('#contact').click(function () {
+        $('#contactForm2').fadeToggle();
+    })
+    $('#close').click(function () {
+        $('#contactForm').fadeToggle();
+    })
+
+    $('#contact2').click(function () {
+        $('#contactForm').fadeToggle();
+    })
+    $('#close2').click(function () {
+        $('#contactForm2').fadeToggle();
+    })
 })
-  // contact form animations
-  
 
-  
+/* Using jQuery fadeToggle function to make the contact form appear */
 
 
-document.getElementById("processArrow").addEventListener('click', function() {
-    if(document.getElementById("processArrow").classList.contains("rotate")){
-    setTimeout(function(){document.getElementById("processArrow").classList.toggle('rotate');},300);
-  document.getElementById("containerp5").classList.toggle('expand');
-    setTimeout(function(){document.getElementById("containerp4").classList.toggle('expand');},300);
-    setTimeout(function(){document.getElementById("containerp3").classList.toggle('expand');},600);
-    setTimeout(function(){document.getElementById("containerp2").classList.toggle('expand');},900);
-    setTimeout(function(){document.getElementById("containerp1").classList.toggle('expand');},1200);
-         
-    
-}  
-    else{
-    document.getElementById("processArrow").classList.toggle('rotate');
-  document.getElementById("containerp1").classList.toggle('expand');
-    setTimeout(function(){document.getElementById("containerp2").classList.toggle('expand');},300);
-    setTimeout(function(){document.getElementById("containerp3").classList.toggle('expand');},600);
-    setTimeout(function(){document.getElementById("containerp4").classList.toggle('expand');},900);
-    setTimeout(function(){document.getElementById("containerp5").classList.toggle('expand');},1200);
-                                                         
-                                                         }})
+/* Making process section animation with text expanding */
+
+document.getElementById("processArrow").addEventListener('click', function () {
+    if(document.getElementById("processArrow").classList.contains("rotate")) {
+        setTimeout(function () { document.getElementById("processArrow").classList.toggle('rotate');
+        }, 300); document.getElementById("containerp5").classList.toggle('expand');
+        setTimeout(function () { document.getElementById("containerp4").classList.toggle('expand');
+        }, 300);
+        setTimeout(function () { document.getElementById("containerp3").classList.toggle('expand');
+        }, 600);
+        setTimeout(function () {  document.getElementById("containerp2").classList.toggle('expand');
+        }, 900);
+        setTimeout(function () { document.getElementById("containerp1").classList.toggle('expand');
+        }, 1200);} 
+    else { document.getElementById("processArrow").classList.toggle('rotate'); document.getElementById("containerp1").classList.toggle('expand');
+        setTimeout(function () { document.getElementById("containerp2").classList.toggle('expand');
+        }, 300);
+        setTimeout(function () { document.getElementById("containerp3").classList.toggle('expand');
+        }, 600);
+        setTimeout(function () { document.getElementById("containerp4").classList.toggle('expand');
+        }, 900);
+        setTimeout(function () { document.getElementById("containerp5").classList.toggle('expand');
+        }, 1200);
+    }
+})
+
+/* Making process section animation with text expanding */
