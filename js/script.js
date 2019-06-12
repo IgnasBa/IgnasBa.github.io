17d58948a71adbe1bbe4eda4192de94f8d30e7f0
IgnasBa <43188272+IgnasBa@users.noreply.github.com>
2019-06-12 12:13:05 +0200

    Revert "Add files via upload"
    This reverts commit 89529bf989e3fcd27ab9ab3d3852bbc259e621fc.

--- js/script.js
+++ js/script.js
@@ -96,171 +96,27 @@ $(function () {
 /* Making process section animation with text expanding */
 
 document.getElementById("processArrow").addEventListener('click', function () {
-    if (document.getElementById("processArrow").classList.contains("rotate")) {
-        setTimeout(function () {
-            document.getElementById("processArrow").classList.toggle('rotate');
+    if(document.getElementById("processArrow").classList.contains("rotate")) {
+        setTimeout(function () { document.getElementById("processArrow").classList.toggle('rotate');
+        }, 300); document.getElementById("containerp5").classList.toggle('expand');
+        setTimeout(function () { document.getElementById("containerp4").classList.toggle('expand');
         }, 300);
-        document.getElementById("containerp5").classList.toggle('expand');
-        setTimeout(function () {
-            document.getElementById("containerp4").classList.toggle('expand');
-        }, 300);
-        setTimeout(function () {
-            document.getElementById("containerp3").classList.toggle('expand');
+        setTimeout(function () { document.getElementById("containerp3").classList.toggle('expand');
         }, 600);
-        setTimeout(function () {
-            document.getElementById("containerp2").classList.toggle('expand');
+        setTimeout(function () {  document.getElementById("containerp2").classList.toggle('expand');
         }, 900);
-        setTimeout(function () {
-            document.getElementById("containerp1").classList.toggle('expand');
-        }, 1200);
-    } else {
-        document.getElementById("processArrow").classList.toggle('rotate');
-        document.getElementById("containerp1").classList.toggle('expand');
-        setTimeout(function () {
-            document.getElementById("containerp2").classList.toggle('expand');
+        setTimeout(function () { document.getElementById("containerp1").classList.toggle('expand');
+        }, 1200);} 
+    else { document.getElementById("processArrow").classList.toggle('rotate'); document.getElementById("containerp1").classList.toggle('expand');
+        setTimeout(function () { document.getElementById("containerp2").classList.toggle('expand');
         }, 300);
-        setTimeout(function () {
-            document.getElementById("containerp3").classList.toggle('expand');
+        setTimeout(function () { document.getElementById("containerp3").classList.toggle('expand');
         }, 600);
-        setTimeout(function () {
-            document.getElementById("containerp4").classList.toggle('expand');
+        setTimeout(function () { document.getElementById("containerp4").classList.toggle('expand');
         }, 900);
-        setTimeout(function () {
-            document.getElementById("containerp5").classList.toggle('expand');
+        setTimeout(function () { document.getElementById("containerp5").classList.toggle('expand');
         }, 1200);
     }
 })
 
 /* Making process section animation with text expanding */
-
-
-var animateHTML = function () {
-    var elems;
-    var windowHeight;
-
-    function init() {
-        elems = document.querySelectorAll('.hidden');
-        windowHeight = window.innerHeight;
-        addEventHandlers();
-        checkPosition();
-    }
-
-    function addEventHandlers() {
-        window.addEventListener('scroll', checkPosition);
-        window.addEventListener('resize', init);
-    }
-
-    function checkPosition() {
-        for (var i = 0; i < elems.length; i++) {
-            var positionFromTop = elems[i].getBoundingClientRect().top;
-            if (positionFromTop - windowHeight <= 0) {
-                elems[i].className = elems[i].className.replace(
-                    'hidden',
-                    'bounce-in-right'
-                );
-            }
-        }
-    }
-    return {
-        init: init
-    };
-};
-animateHTML().init();
-var animateHTML2 = function () {
-    var elems;
-    var windowHeight;
-
-    function init() {
-        elems = document.querySelectorAll('.hidden2');
-        windowHeight = window.innerHeight;
-        addEventHandlers();
-        checkPosition();
-    }
-
-    function addEventHandlers() {
-        window.addEventListener('scroll', checkPosition);
-        window.addEventListener('resize', init);
-    }
-
-    function checkPosition() {
-        for (var i = 0; i < elems.length; i++) {
-            var positionFromTop = elems[i].getBoundingClientRect().top;
-            if (positionFromTop - windowHeight <= 0) {
-                elems[i].className = elems[i].className.replace(
-                    'hidden2',
-                    'bounce-in-left'
-                );
-            }
-        }
-    }
-    return {
-        init: init
-    };
-};
-animateHTML2().init();
-var animateHTML3 = function () {
-    var elems;
-    var windowHeight;
-
-    function init() {
-        elems = document.querySelectorAll('.hidden3');
-        windowHeight = window.innerHeight;
-        addEventHandlers();
-        checkPosition();
-    }
-
-    function addEventHandlers() {
-        window.addEventListener('scroll', checkPosition);
-        window.addEventListener('resize', init);
-    }
-
-    function checkPosition() {
-        for (var i = 0; i < elems.length; i++) {
-            var positionFromTop = elems[i].getBoundingClientRect().top;
-            if (positionFromTop - windowHeight <= 0) {
-                elems[i].className = elems[i].className.replace(
-                    'hidden3',
-                    'bounce-in-top'
-                );
-            }
-        }
-    }
-    return {
-        init: init
-    };
-};
-animateHTML3().init();
-var animateHTML4 = function () {
-        var elems;
-        var windowHeight;
-
-        function init() {
-            elems = document.querySelectorAll('.hidden4');
-            windowHeight = window.innerHeight;
-            addEventHandlers();
-            checkPosition();
-        }
-
-        function addEventHandlers() {
-            window.addEventListener('scroll', checkPosition);
-            window.addEventListener('resize', init);
-        }
-
-        function checkPosition() {
-            for (var i = 0; i < elems.length; i++) {
-                var positionFromTop = elems[i].getBoundingClientRect().top;
-                if (positionFromTop - windowHeight <= 0) {
-                    elems[i].className = elems[i].className.replace(
-                        'hidden4',
-                        'bounce-in-bottom'
-                    );
-                }
-               
-
-                }
-            }
-            return {
-                init: init
-            };
-        };
-        animateHTML4().init();
